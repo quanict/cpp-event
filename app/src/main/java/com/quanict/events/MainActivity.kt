09ff068a17f6f76e7c1e9de86bde6e6e010e7543
quanict <hongquan2712@gmail.com>
2024-01-28 14:19:31 +0700

    add source

---
+++ app/src/main/java/com/quanict/events/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.quanict.events
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.quanict.events.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'events' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'events' library on application startup.
+        init {
+            System.loadLibrary("events")
+        }
+    }
+}
